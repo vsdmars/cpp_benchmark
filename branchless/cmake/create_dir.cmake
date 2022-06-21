31a082b8ab98007dd7741b3c34b8553b4e7fd26c
vsdmars <vsdmars@gmail.com>
2022-06-20 17:04:48 -0700

    update build config

--- branchless/cmake/create_dir.cmake
+++ branchless/cmake/create_dir.cmake
@@ -4,6 +4,6 @@ SET(BIN_TEST_DIR "${PROJECT_BINARY_DIR}/tests")
 
 SET(BUILD_DIRS ${BIN_DIR} ${BIN_TEST_DIR})
 
-foreach(d ${LRUC_DIRS})
+foreach(d ${BUILD_DIRS})
    file(MAKE_DIRECTORY "${d}")
 endforeach(d)
